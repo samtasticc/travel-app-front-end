d3c6845a075de773c572e5009afec468e1d67aeb
samtasticc <samantha.henesy@gmail.com>
2024-12-07 17:37:19 -0500

    created sign up form, import form to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm';
 
 const App = () => {
   const [user, setUser] = useState(null)
