80c8dc57066f66e0dc46106a1d3b96bf7d6a8c71
samtasticc <samantha.henesy@gmail.com>
2024-12-08 17:17:54 -0500

    add auth to authService.js for sign up page

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,23 @@
+const BACKEND_URL = 'http://localhost:3000'
+
+const signup = async (FormData) => {
+    try {
+        const res = await fetch(`${BACKEND_URL}/users/signup`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData) 
+        })
+        const json = await res.json()
+        if (json.err) {
+            throw new Error(json.err)
+        }
+        return json
+    } catch (err) {
+        throw err
+    }
+}
+
+
 const signin = async (user) => {
     try {
         const res = await fetch(`${BACKEND_URL}/users/signin`, {
