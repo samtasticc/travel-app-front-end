dfb57f641db4d793f49aa0807710a7c605b13b1b
samtasticc <samantha.henesy@gmail.com>
2024-12-08 17:13:08 -0500

    import and render the sign in form.

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
+import SigninForm from './components/SigninForm/SigninForm';
 
 const App = () => {
   const [user, setUser] = useState(null)
 
   return (
     <>
-      <NavBar user={user} />
+      <NavBar user={user} handleSignout={handleSignout} />
       <Routes>
-      <Route path="/signup" element={<SignupForm setUser={setUser} />} />
         { user ? (
           <Route path='/' element={<Dashboard user={user} />} />
         ) : (
           <Route path='/' element={<Landing />} />  
         )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
       </Routes>
     </>
   )
