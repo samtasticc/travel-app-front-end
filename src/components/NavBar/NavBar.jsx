d5c8ec32593428a3e99cb6a94784a62a13802387
samtasticc <samantha.henesy@gmail.com>
2024-12-08 20:39:33 -0500

    add link to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,7 @@ const NavBar = ({ user, handleSignout }) => {
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/travels">Travel Wishlist</Link></li>
             <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
           </ul>
         </nav>
