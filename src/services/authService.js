318388002cc1063cef7beb5b4bb4b07652b8a95f
samtasticc <samantha.henesy@gmail.com>
2024-12-08 19:09:44 -0500

    set token for signup

--- src/services/authService.js
+++ src/services/authService.js
@@ -11,7 +11,11 @@ const signup = async (FormData) => {
         if (json.err) {
             throw new Error(json.err)
         }
-        return json
+        if(json.token) {
+            localStorage.setItem('token', json.token)
+            const user = JSON.parse(atob(json.token.split('.')[1]));
+            return user;
+        }
     } catch (err) {
         throw err
     }
@@ -20,24 +24,27 @@ const signup = async (FormData) => {
 
 const signin = async (user) => {
     try {
-        const res = await fetch(`${BACKEND_URL}/users/signin`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(user)
-        })
-        const json = await res.json()
-
-        if (json.error) { 
-            throw new Error(json.error)
-        }
-
-        if (json.token) {
-            const user = JSON.parse(atob(json.toke.split('.')[1]))
-            return user
-        }
+      const res = await fetch(`${BACKEND_URL}/users/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      })
+      const json = await res.json()
+  
+      if (json.token) {
+        localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+  
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+  
+        return user
+      }
+      if (json.err) {
+        throw new Error(json.err)
+      }
     } catch (err) {
-        throw err
+      console.log(err)
+      throw err
     }
-}
+  }
 
 export { signup, signin }
