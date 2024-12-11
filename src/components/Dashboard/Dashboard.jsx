56b2d0b79de670831bf416a9d8774bf275a08b05
samtasticc <samantha.henesy@gmail.com>
2024-12-10 20:58:56 -0500

    update Dashboard and Landing files

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ const Dashboard = ({ user }) => {
     return (
         <main>
             <h1>Welcome, {user.username}, to Jet Set Journal!</h1>
+            <p>Where your travel aspirations take flight!</p>
             <p>
                Jet Set Journal is a travel wishlist tracker to help users map out and maintain their future travel plans. Users can create an account, log in, and create their next vacation. Each travel plan allows the user to add a title, date, Country, state/territory, flight options, hotel options, restaurant options, and activity ideas.
             </p>
